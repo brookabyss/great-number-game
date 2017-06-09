373ab267bf0ba7f868ea7de4865b07dbd8639d2f
Brook Kebede <brookabyss@gmail.com>
2017-06-08 17:05:54 -0700

    Number checking working , had trouble with number from input being string and compared with an int. The styling needs work but the functionality works.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
 app=Flask(__name__)
 app.secret_key="1234"
 @app.route('/')
@@ -7,4 +7,13 @@ def index():
     session["num"]=random.randrange(1,100)
     print session["num"]
     return render_template("index.html")
+@app.route('/number', methods=['POST'])
+def checker():
+   session['user']=int(request.form['user'])
+   return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.pop('user')
+    return redirect('/')
 app.run(debug=True)
